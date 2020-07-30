00c0d13ed23824e720be68113f06c87fb2bde07b
JAUSLIN Raphaël <raphael.jauslin@unine.ch>
2020-07-30 11:15:40 +0200

    first commit

---
+++ R/zzz.R
@@ -0,0 +1,18 @@
+#' zzz
+#'
+#' @description 
+#' 
+#' add DynLib and Rcpp sourceCpp
+#' 
+#' @encoding UTF-8
+#' @import Matrix
+#' 
+#' @useDynLib Sampling, .registration = TRUE
+#' 
+#' 
+#' @importFrom Rcpp sourceCpp
+#' @export
+#' 
+zzz <- function(){
+  
+}
